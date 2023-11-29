086fa2b5a8ece345b779bdb9060cfa39c551bae5
quachconghoang <hoangqc@vnu.edu.vn>
2023-11-29 18:00:56 +1100

    Update install_HPC.sh

---
+++ docker/install_HPC.sh
@@ -0,0 +1,16 @@
+singularity shell --nv glue-uts.sif
+
+### Install LightGlue
+# git clone https://github.com/cvg/LightGlue.git
+# cd LightGlue
+# python3 -m pip install -e .
+
+### Install Glue Factory
+### cd ..
+### git clone https://github.com/quachconghoang/glue-factory.git
+### cd ../glue-factory
+python3 -m pip install -e .
+
+### check data folder
+### check ... homography folder
+### check ... megadepth folder
